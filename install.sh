872eb149962b0e9574fffbe613ec79ea92de581f
LucasBonny <123037864+LucasBonny@users.noreply.github.com>
2024-03-13 15:31:26 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 {
     mkdir /root/BOT
     wget https://raw.githubusercontent.com/LucasBonny/atlas/main/port80.py
-    "* * * * * python3 /root/BOT/port80.py >> /root/BOT/port80.log" | crontab -
+    echo "* * * * * python3 /root/BOT/port80.py >> /root/BOT/port80.log" | crontab -
     screen -S port80proxyDT -X stuff "echo while true; do ulimit -n 999999 && proxy --port 80; done\n"
     mv port80.py /root/BOT
+    clear
+    rm install.sh
 }
